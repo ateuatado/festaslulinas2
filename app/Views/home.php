bdcaae5650583c498250025d1879a5e17a0c1980
ateuatado <anjosdoamor@gmail.com>
2025-01-25 22:33:49 -0300

    trabalho desse sábado

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,13 +1,152 @@
 <?=view('template/header'); ?>
- 
-<div class="container p-0"> 
-    <div id="lulina2">
-        <br><br><br><br><br><br><br>
-        <div class="d-inline p-2 ms-5 my-5 display-1 bg-light">Bem-vindo às Festas Lulinas!</div>
-        <p>Aqui você encontra tudo sobre as melhores festas juninas da região!</p>
+<div class="container">
+    <div class="row my-3">
+        <div class="col-3">
+            <div class="card card-transparente shadow mb-3 rounded">
+                <div class="card-body p-3 ">
+                    <div class="row">
+                        <div class="col-4 text-center text-right ">
+                           <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#5f6368"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 13h-3c-.55 0-1 .45-1 1v3c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-3c0-.55-.45-1-1-1zm0-10v1H8V3c0-.55-.45-1-1-1s-1 .45-1 1v1H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-1V3c0-.55-.45-1-1-1s-1 .45-1 1zm2 17H6c-.55 0-1-.45-1-1V9h14v10c0 .55-.45 1-1 1z"/></svg>
+                        </div>
+                        <div class="col-8 text-left text-lead text-secondary">
+                            <a href="<?=base_url('vai') ?>" class="text-decoration-none d-block">
+                                Cadastre Sua Festa Em breve...
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card card-transparente shadow mb-3 rounded">
+                <div class="card-body p-3 ">
+                    <div class="row">
+                        <div class="col-4 text-center text-right ">
+                           <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#5f6368"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12.72 2.03C6.63 1.6 1.6 6.63 2.03 12.72 2.39 18.01 7.01 22 12.31 22H16c.55 0 1-.45 1-1s-.45-1-1-1h-3.67c-3.73 0-7.15-2.42-8.08-6.03-1.49-5.8 3.91-11.21 9.71-9.71C17.58 5.18 20 8.6 20 12.33v1.1c0 .79-.71 1.57-1.5 1.57s-1.5-.78-1.5-1.57v-1.25c0-2.51-1.78-4.77-4.26-5.12-3.4-.49-6.27 2.45-5.66 5.87.34 1.91 1.83 3.49 3.72 3.94 1.84.43 3.59-.16 4.74-1.33.89 1.22 2.67 1.86 4.3 1.21 1.34-.53 2.16-1.9 2.16-3.34v-1.09c0-5.31-3.99-9.93-9.28-10.29zM12 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"/></svg>
+                        </div>
+                        <div class="col-8 text-left text-lead text-secondary">
+                            <a href="<?=base_url('vai') ?>" class="text-decoration-none d-block">
+                                Entre em Contato Conosco
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card card-transparente shadow mb-3 rounded">
+                <div class="card-body p-3 ">
+                    <div class="row">
+                        <div class="col-4 text-center text-right ">
+                           <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="48px" viewBox="0 0 24 24" width="48px" fill="#5f6368"><g><rect fill="none" height="24" width="24" x="0"/></g><g><path d="M18.15,1.35l-4,4C14.05,5.45,14,5.57,14,5.71v8.17c0,0.43,0.51,0.66,0.83,0.37l4-3.6c0.11-0.09,0.17-0.23,0.17-0.37V1.71 C19,1.26,18.46,1.04,18.15,1.35z M22.47,5.2C22,4.96,21.51,4.76,21,4.59v12.03C19.86,16.21,18.69,16,17.5,16 c-1.9,0-3.78,0.54-5.5,1.58V5.48C10.38,4.55,8.51,4,6.5,4C4.71,4,3.02,4.44,1.53,5.2C1.2,5.36,1,5.71,1,6.08v12.08 c0,0.76,0.81,1.23,1.48,0.87C3.69,18.4,5.05,18,6.5,18c2.07,0,3.98,0.82,5.5,2c1.52-1.18,3.43-2,5.5-2c1.45,0,2.81,0.4,4.02,1.04 C22.19,19.4,23,18.93,23,18.17V6.08C23,5.71,22.8,5.36,22.47,5.2z"/></g></svg>
+                        </div>
+                        <div class="col-8 text-left text-lead text-secondary">
+                            <a href="<?=base_url('vai') ?>" class="text-decoration-none d-block">
+                                Conheça Nossa História
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card card-transparente shadow mb-3 rounded">
+                <div class="card-body p-3 ">
+                    <div class="row">
+                        <div class="col-4 text-center text-right ">
+                           <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="48px" viewBox="0 0 24 24" width="48px" fill="#5f6368"><g><rect fill="none" height="24" width="24"/></g><g><path d="M12,2C6.48,2,2,6.48,2,12s4.48,10,10,10s10-4.48,10-10S17.52,2,12,2z M12.88,17.76v0.36c0,0.48-0.39,0.88-0.88,0.88h0 c-0.48,0-0.88-0.39-0.88-0.88v-0.42c-0.63-0.15-1.93-0.61-2.69-2.1c-0.23-0.44-0.01-0.99,0.45-1.18l0.07-0.03 c0.41-0.17,0.87,0,1.08,0.39c0.32,0.61,0.95,1.37,2.12,1.37c0.93,0,1.98-0.48,1.98-1.61c0-0.96-0.7-1.46-2.28-2.03 c-1.1-0.39-3.35-1.03-3.35-3.31c0-0.1,0.01-2.4,2.62-2.96V5.88C11.12,5.39,11.52,5,12,5h0c0.48,0,0.88,0.39,0.88,0.88v0.37 c1.07,0.19,1.75,0.76,2.16,1.3c0.34,0.44,0.16,1.08-0.36,1.3l0,0C14.32,9,13.9,8.88,13.66,8.57c-0.28-0.38-0.78-0.77-1.6-0.77 c-0.7,0-1.81,0.37-1.81,1.39c0,0.95,0.86,1.31,2.64,1.9c2.4,0.83,3.01,2.05,3.01,3.45C15.9,17.17,13.4,17.67,12.88,17.76z"/></g></svg>
+                        </div>
+                        <div class="col-8 text-left text-lead text-secondary">
+                            <a href="<?=base_url('vai') ?>" class="text-decoration-none d-block">
+                                Contribua com as Festas Lulinas
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Carrossel -->
+<div class="container-fluid p-0 my-1">
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?php echo base_url('img/lulina1.jpg'); ?>" class="d-block w-100" alt="Festa Lulina 2022">
+                <div class="carousel-caption">
+                    <h3>Festa Lulina 2022</h3>
+                    <p>A retomada da esperança!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo base_url('img/lulina2.jpg'); ?>" class="d-block w-100" alt="Festa Lulina 2023">
+                <div class="carousel-caption">
+                    <h3>Festa Lulina 2023</h3>
+                    <p>A consolidação da reconstrução!</p>
+                </div>
+            </div>
+            <div class="carousel-item row-bgazulalpha">
+                <img src="<?php echo base_url('img/lulina3.jpg'); ?>" class="d-block w-100" alt="Festa Lulina 2024">
+                <div class="carousel-caption">
+                    <h3>Festa Lulina 2024</h3>
+                    <p>União e reconstrução em todo lugar!</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Próximo</span>
+        </button>
     </div>
 </div>
 
+<hr class="mt-5 mb-5">
 
+    <div class="row row-bgazulalpha30 p-3 rounded">
+        <div class="col-md-12 text-center row-bgazulalpha30 rounded p-2">
+            <h2>Nossos Colaboradores</h2>
+            <p class="lead">
+                Conheça as pessoas incríveis que fazem as Festas Lulinas acontecerem!
+            </p>
+            <p>
+                Dedicação, empenho e amor pela cultura popular são apenas algumas das qualidades que definem nossos colaboradores. Cada um deles contribui de forma única para o sucesso das nossas festas, e somos imensamente gratos por seu trabalho e compromisso.
+            </p>
+        </div>
+    </div>
+
+<div class="row row-bgazulalpha30 p-3 mb-5"> 
+
+
+
+    <div class="col-md-3 col-sm-6 text-center">
+        <img src="<?php echo base_url('img/gleisi.jpg'); ?>" alt="Colaborador 1" class="rounded-circle img-fluid" style="width: 150px; height: 150px; object-fit: cover;">
+        <h4 class="mt-3">Gleisi Hoffman</h4>
+        <p>Pres. do PT nacional. Mulher incrível, combatente pela democracia brasileira.</p>
+    </div>
+
+    <div class="col-md-3 col-sm-6 text-center">
+        <img src="<?php echo base_url('img/lula.jpg'); ?>" alt="Colaborador 2" class="rounded-circle img-fluid" style="width: 150px; height: 150px; object-fit: cover;">
+        <h4 class="mt-3">Pres. Lula</h4>
+        <p>O maior presidente da histório do Brasil</p>
+    </div>
+
+    <div class="col-md-3 col-sm-6 text-center">
+        <img src="<?php echo base_url('img/KikoSeleguim.jpg'); ?>" alt="Colaborador 3" class="rounded-circle img-fluid" style="width: 150px; height: 150px; object-fit: cover;">
+        <h4 class="mt-3">Kiko Celeguin</h4>
+        <p>Pres. PT São Paulo.</p>
+    </div>
+
+    <div class="col-md-3 col-sm-6 text-center">
+        <img src="<?php echo base_url('img/padrejulio.jpg'); ?>" alt="Colaborador 4" class="rounded-circle img-fluid" style="width: 150px; height: 150px; object-fit: cover;">
+        <h4 class="mt-3">Padre Julio Lancellot</h4>
+        <p>Incansável servo de Deus. Ser humano sensível e combatente voraz contra a fome e a miséria.</p>
+    </div>
+</div>
+
+<br><br><br><br>
 
 <?=view('template/footer'); ?>
